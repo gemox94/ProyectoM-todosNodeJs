d7f3dc7775cb9b4a7bae9c92c707f9dc3e18daf8
aster <aster.december@gmail.com>
2017-02-19 13:10:03 -0600

    routes added

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,31 @@ router.get('/', function(req, res, next) {
   res.render('home', { title: 'Express' });
 });
 
+router.get('/biseccion'function(req,res,next){
+	res.render('biseccion');
+});
+router.get('/polinomios'function(req,res,next){
+	res.render('polinomios');
+});
+router.get('/secante'function(req,res,next){
+	res.render('secante');
+});
+router.get('/reglaF'function(req,res,next){
+	res.render('reglaF');
+});
+router.get('/newton'function(req,res,next){
+	res.render('newton');
+});
+router.get('/newtonM'function(req,res,next){
+	res.render('newtonM');
+});
+router.get('/muller'function(req,res,next){
+	res.render('muller');
+});
+router.get('/steffensen'function(req,res,next){
+	res.render('steffensen');
+});
+
+
+
 module.exports = router;
