6cd9a1b9460461e986e2183a44860949cdd502b0
aster <aster.december@gmail.com>
2017-02-19 13:21:16 -0600

    added angular

--- routes/index.js
+++ routes/index.js
@@ -2,32 +2,32 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/',function(req, res, next) {
   res.render('home', { title: 'Express' });
 });
 
-router.get('/biseccion'function(req,res,next){
+router.get('/biseccion',function(req,res,next){
 	res.render('biseccion');
 });
-router.get('/polinomios'function(req,res,next){
+router.get('/polinomios',function(req,res,next){
 	res.render('polinomios');
 });
-router.get('/secante'function(req,res,next){
+router.get('/secante',function(req,res,next){
 	res.render('secante');
 });
-router.get('/reglaF'function(req,res,next){
+router.get('/reglaF',function(req,res,next){
 	res.render('reglaF');
 });
-router.get('/newton'function(req,res,next){
+router.get('/newton',function(req,res,next){
 	res.render('newton');
 });
-router.get('/newtonM'function(req,res,next){
+router.get('/newtonM',function(req,res,next){
 	res.render('newtonM');
 });
-router.get('/muller'function(req,res,next){
+router.get('/muller',function(req,res,next){
 	res.render('muller');
 });
-router.get('/steffensen'function(req,res,next){
+router.get('/steffensen',function(req,res,next){
 	res.render('steffensen');
 });
 
